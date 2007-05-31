6adac63643c69b2fbd32b66e757299f183df6dc8
Dan Armstrong <dan@aoindustries.com>
2007-05-31 15:17:16 +0000

    Added virtual dedicated and virtual managed signup forms

--- aoindustries/package_categories-data1.sql
+++ aoindustries/package_categories-data1.sql
@@ -4,6 +4,8 @@ insert into package_categories values('aoserv', 'AOServ Software');
 insert into package_categories values('backup', 'Backup Service');
 insert into package_categories values('sysadmin', 'System Administration');
 insert into package_categories values('colocation', 'Colocation');
+insert into package_categories values('virtual_dedicated', 'Virtual Dedicated Servers');
+insert into package_categories values('virtual_managed', 'Virtual Managed Servers');
 insert into package_categories values('dedicated', 'Dedicated Servers');
 insert into package_categories values('managed', 'Managed Servers');
 insert into package_categories values('reseller', 'Reseller');

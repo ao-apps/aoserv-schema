0448d3e4e49a1952d8ba18bd2785eedcf2acfdc2
Dan Armstrong <dan@aoindustries.com>
2008-01-06 16:49:04 +0000

    Working on CentOS 5 distribution checks, added prelinked binary/library verification support.

--- aoindustries/distro_file_types-data1.sql
+++ aoindustries/distro_file_types-data1.sql
@@ -1,6 +1,7 @@
 COPY "distro_file_types" FROM stdin;
 config	Configuration files must exist and have matching permissions
 no_recurse	System files that must exactly match, but will not be recursively verified
+prelink	System files that are prelinked and must exactly match the distribution data
 system	System files must exactly match the distribution data
 user	User directories are scanned for problem files
 \.

e40c354e67ed6990a7548ab72978b51ed90e7ffc
Dan Armstrong <dan@aoindustries.com>
2010-06-18 08:31:22 +0000

    Working on AOServ 2.0

--- aoindustries/aoserv_role_permissions-data1.sql
+++ aoindustries/aoserv_role_permissions-data1.sql
@@ -1,18 +1,21 @@
 select setval('aoserv_role_permissions_pkey_seq', 1, true);
 begin;
 -- Read-Only
--- None
+insert into aoserv_role_permissions values(default, 1, 'get_ticket_details');
+insert into aoserv_role_permissions values(default, 1, 'get_transaction_description');
 -- Master User
 insert into aoserv_role_permissions select nextval('aoserv_role_permissions_pkey_seq'), 2, "name" from aoserv_permissions; -- All permissions
 -- AOServ Daemon
 insert into aoserv_role_permissions values(default, 3, 'add_ticket');
 -- AOWeb Struts
 insert into aoserv_role_permissions values(default, 4, 'add_ticket');
+insert into aoserv_role_permissions values(default, 4, 'add_transaction'); -- Accepts credit card payments
 -- Business Administrator
 insert into aoserv_role_permissions values(default, 5, 'add_ticket_annotation');
 insert into aoserv_role_permissions values(default, 5, 'add_ticket');
 insert into aoserv_role_permissions values(default, 5, 'edit_ticket');
 insert into aoserv_role_permissions values(default, 5, 'get_ticket_details');
 insert into aoserv_role_permissions values(default, 5, 'set_mysql_user_password');
+insert into aoserv_role_permissions values(default, 5, 'get_transaction_description');
 -- AWStats Proxy
 commit;

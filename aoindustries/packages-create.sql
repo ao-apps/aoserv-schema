5e28377b75121af35fdd72fdef14bbd6bd8b8628
Dan Armstrong <dan@aoindustries.com>
2012-02-05 17:00:39 +0000

    Reverting back to version 1.61

---
+++ aoindustries/packages-create.sql
@@ -0,0 +1,30 @@
+create sequence packages_pkey_seq cycle;
+grant all on packages_pkey_seq to aoadmin;
+grant select, update on packages_pkey_seq to aoserv_app;
+
+create table packages (
+  pkey integer
+    default nextval('packages_pkey_seq')
+    constraint packages_pkey primary key,
+  name text
+    not null
+    unique,
+  accounting text
+    not null,
+  package_definition integer
+    not null,
+  created timestamp
+    not null
+    default now(),
+  created_by text
+    not null,
+  disable_log integer,
+  email_in_burst integer,
+  email_in_rate float4,
+  email_out_burst integer,
+  email_out_rate float4,
+  email_relay_burst integer,
+  email_relay_rate float4
+);
+grant all on packages to aoadmin;
+grant select, insert, update, delete on packages to aoserv_app;
---
+++ aoindustries/packages-test.sql
@@ -0,0 +1,33 @@
+begin;
+insert into packages values(
+    2,
+	'TEST',
+	'TEST',
+	1,
+	'2004-11-28 19:14:02.812879-06',
+	'orion',
+	null,
+	null,
+	null,
+	null,
+	null,
+	null,
+	null
+);
+insert into packages values(
+    4,
+	'NMW',
+	'NMW',
+	1,
+	'2006-03-21 13:11:00-06',
+	'orion',
+	null,
+	null,
+	null,
+	null,
+	null,
+	null,
+	null
+);
+SELECT setval ('"packages_pkey_seq"', 5, true);
+commit;

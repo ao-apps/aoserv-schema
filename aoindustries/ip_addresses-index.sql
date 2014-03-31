46b9bb6af389f981cf8265e7992aa4b2a75dd358
Dan Armstrong <dan@aoindustries.com>
2014-03-31 20:54:16 +0000

    Trying to catch up and clean up tasks.

--- aoindustries/ip_addresses-index.sql
+++ aoindustries/ip_addresses-index.sql
@@ -4,3 +4,4 @@ create index ip_addresses_package_idx on ip_addresses (
 create index ip_addresses_net_device_idx on ip_addresses (
   net_device
 );
+create index ip_addresses_pad_ip_address_idx on ip_addresses (pad_ip_address(ip_address));

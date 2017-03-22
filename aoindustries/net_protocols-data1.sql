2c6cb94db04b12db7ad7a63121a3aa2c58d58515
Dan Armstrong <dan@aoindustries.com>
2017-03-22 21:23:03 +0000

    This table is now unnecessary.

--- aoindustries/net_protocols-data1.sql
+++ aoindustries/net_protocols-data1.sql
@@ -1,3 +1,5 @@
+-- TODO: Make capital and include all of the names in Protocol ao-net-types enumeration
+-- TODO: Or just do away with this table entirely and rely on self-validating types
 COPY "net_protocols" FROM stdin;
 udp
 tcp

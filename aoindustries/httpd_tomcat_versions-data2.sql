4e40cd0651bcf2851f8ddabacff4b20f87a4bfe9
Dan Armstrong <dan@aoindustries.com>
2017-05-12 02:59:59 +0000

    Tomcat installed to CentOS 7.

--- aoindustries/httpd_tomcat_versions-data2.sql
+++ aoindustries/httpd_tomcat_versions-data2.sql
@@ -7,3 +7,8 @@ insert into httpd_tomcat_versions values(3233, '/usr/apache/jakarta/tomcat/3.2',
 insert into httpd_tomcat_versions values(3234, '/usr/apache/jakarta/tomcat/4.1', true);
 insert into httpd_tomcat_versions values(3235, '/usr/apache/jakarta/tomcat/5.5', true);
 insert into httpd_tomcat_versions values(3228, '/opt/apache-tomcat-5.5', true);
+-- CentOS 5
+-- CentOS 7
+insert into httpd_tomcat_versions values(3358, '/opt/apache-tomcat-6.0', true);
+insert into httpd_tomcat_versions values(3359, '/opt/apache-tomcat-7.0', true);
+insert into httpd_tomcat_versions values(3360, '/opt/apache-tomcat-8.0', true);

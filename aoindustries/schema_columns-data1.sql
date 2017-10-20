7ce304309498f38e01f76f6bac2e01417f4d89fc
Dan Armstrong <dan@aoindustries.com>
2017-10-20 05:41:39 +0000

    Fixed typo in column description.

--- aoindustries/schema_columns-data1.sql
+++ aoindustries/schema_columns-data1.sql
@@ -619,7 +619,7 @@ begin;
 \echo disable_log
 insert into schema_columns select nextval('schema_columns_pkey_seq'), 'disable_log', 'pkey', 0, 'pkey', false, true, false, 'a generated primary key', '1.0a100', null;
 insert into schema_columns select nextval('schema_columns_pkey_seq'), 'disable_log', 'time', 1, 'time', false, false, false, 'the time the stuff was disabled', '1.0a100', null;
-insert into schema_columns select nextval('schema_columns_pkey_seq'), 'disable_log', 'accounting', 2, 'accounting', false, false, false, 'the business whos resources are being disabled', '1.0a100', null;
+insert into schema_columns select nextval('schema_columns_pkey_seq'), 'disable_log', 'accounting', 2, 'accounting', false, false, false, 'the business whose resources are being disabled', '1.0a100', null;
 insert into schema_columns select nextval('schema_columns_pkey_seq'), 'disable_log', 'disabled_by', 3, 'username', false, false, false, 'the person who disabled the accounts', '1.0a100', null;
 insert into schema_columns select nextval('schema_columns_pkey_seq'), 'disable_log', 'disable_reason', 4, 'string', true, false, false, 'the optional reason the accounts were disabled', '1.0a100', null;
 commit;

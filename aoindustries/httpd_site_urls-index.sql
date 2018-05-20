bcb2a56e4b05d3902164273d1ea63a61721548d1
Dan Armstrong <dan@aoindustries.com>
2018-05-20 20:12:57 +0000

    httpd_site_bind is unique for primary URLs.

--- aoindustries/httpd_site_urls-index.sql
+++ aoindustries/httpd_site_urls-index.sql
@@ -1,3 +1,6 @@
 create index httpd_site_urls_httpd_site_bind_idx on httpd_site_urls (
   httpd_site_bind
 );
+create unique index httpd_site_urls_is_primary_uni on httpd_site_urls (
+  httpd_site_bind
+) where is_primary;

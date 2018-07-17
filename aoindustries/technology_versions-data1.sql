99caa99b85411fdf2002878fe4139ede9cfde4c5
Dan Armstrong <dan@aoindustries.com>
2018-07-17 11:35:43 +0000

    Added support for PHP 7.0.

--- aoindustries/technology_versions-data1.sql
+++ aoindustries/technology_versions-data1.sql
@@ -1385,6 +1385,7 @@ COPY "technology_versions" FROM stdin;
 3242	php	4.4.4	2006-10-12 18:50:00-05	orion	45
 3243	php	5.1.6	2006-10-12 18:58:00-05	orion	45
 3373	php	7.0.0	2018-07-17 06:22:41.987077+00	orion	70
+3374	php	7.1.0	2018-07-17 11:34:17.801647+00	orion	70
 14	postgresql	7.1.3	2006-06-22 10:16:00-05	orion	45
 15	postgresql	7.2.7	2006-06-22 10:17:00-05	orion	45
 16	postgresql	7.3.15	2006-06-22 10:18:00-05	orion	45
@@ -1422,4 +1423,4 @@ COPY "technology_versions" FROM stdin;
 3357	MySQL	5.7.17	2017-04-03 04:06:29.419632+00	orion	70
 3372	MySQL	8.0.0	2018-07-15 19:32:37.066566+00	orion	70
 \.
-SELECT setval ('"technology_versions_pkey_seq"', 3374, true);
+SELECT setval ('"technology_versions_pkey_seq"', 3375, true);

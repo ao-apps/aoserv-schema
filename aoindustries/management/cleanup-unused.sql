a83efd11a9a7d6626d60aca484a4a77b2c52832c
Dan Armstrong <dan@aoindustries.com>
2018-11-12 21:39:00 +0000

    Working on unused SSL certificate removal.

--- aoindustries/management/cleanup-unused.sql
+++ aoindustries/management/cleanup-unused.sql
@@ -1,24 +1,29 @@
+-- See http://localhost:8080/docs/ao/management/remove/unused#procedure
 create or replace view
-  management."cleanup-unused"
+  management."remove-unused"
 as
 -- TODO: cyrus_imapd_servers without any cyrus_imapd_binds
--- TODO: email_domains with no addresses
 -- TODO: email_lists with no addresses
+-- TODO: email_domains with no addresses
 -- TODO: email_pipes with no addresses
--- TODO: httpd_servers
 -- TODO: httpd_shared_tomcats
+-- TODO: httpd_servers
 -- TODO: mysql_databases (without any mysql_db_users)
 -- TODO: mysql_server_users (without any mysql_db_users)
 -- TODO: mysql_users (orphaned)
 -- mysql_servers
-          (select 'mysql_servers', count(*) as unused from management."remove-mysql-server" where num_databases=0 or num_users=0)
+          (select 'mysql_servers', count(*) as unused from management."remove-mysql-server"
+           where num_databases=0 or num_users=0)
 -- TODO: postgres_databases (without any postgres_users in same business?/package?)
 -- TODO: postgres_server_users (without any postgres_databases in same business?/package?)
 -- TODO: postgres_users (orphaned)
 -- postgres_servers
-union all (select 'postgres_servers', count(*) as unused from management."remove-postgresql-server" where num_databases=0 or num_users=0)
+union all (select 'postgres_servers', count(*) as unused from management."remove-postgresql-server"
+           where num_databases=0 or num_users=0)
 -- TODO: sendmail_servers without any sendmail_binds
--- TODO: ssl_certificates (once have auto-cleanup within aoserv-daemon)
+-- ssl_certificates
+union all (select 'ssl_certificates', count(*) as unused from management."remove-ssl-certificate"
+           where num_cyrus_imapd_binds=0 and num_cyrus_imapd_servers=0 and num_httpd_site_binds=0 and num_sendmail_servers=0)
 -- TODO: firewalld_zones
 -- TODO: ip_addresses
 -- TODO: ftp_guest_users
@@ -26,7 +31,8 @@ union all (select 'postgres_servers', count(*) as unused from management."remove
 -- TODO: linux_server_groups (orphaned or with no linux_group_accounts)
 -- TODO: linux_groups (orphaned or with no linux_group_accounts)
 -- TODO: usernames (orphaned)
+-- TODO: business_servers (access to a server but without any resources - useful?)
 ;
 
-revoke all on management."cleanup-unused" from aoadmin;
-grant select on management."cleanup-unused" to aoadmin;
+revoke all on management."remove-unused" from aoadmin;
+grant select on management."remove-unused" to aoadmin;
---
+++ aoindustries/management/remove-ssl-certificate.sql
@@ -0,0 +1,26 @@
+-- See http://localhost:8080/docs/ao/management/remove/ssl-certificate#procedure
+create or replace view
+  management."remove-ssl-certificate"
+as
+select
+  (select count(*) from public.cyrus_imapd_binds   cib where cib.certificate       = sc.pkey) as num_cyrus_imapd_binds,
+  (select count(*) from public.cyrus_imapd_servers cis where cis.certificate       = sc.pkey) as num_cyrus_imapd_servers,
+  (select count(*) from public.httpd_site_binds    hsb where hsb.certificate       = sc.pkey) as num_httpd_site_binds,
+  (select count(*) from public.sendmail_servers    ss  where ss.server_certificate = sc.pkey
+                                                          or ss.client_certificate = sc.pkey) as num_sendmail_servers,
+  ao.hostname as "SERVER",
+  case when osv.operating_system='centos' then 'CentOS' else osv.operating_system end
+    || ' ' || osv.version_number as "OS",
+  sc.certbot_name as "CERTBOT_NAME",
+  case when sc.certbot_name is null then sc.key_file   else null end as "KEY_FILE",
+  case when sc.certbot_name is null then sc.csr_file   else null end as "CSR_FILE",
+  case when sc.certbot_name is null then sc.cert_file  else null end as "CERT_FILE",
+  case when sc.certbot_name is null then sc.chain_file else null end as "CHAIN_FILE"
+from
+             public.ssl_certificates          sc
+  inner join public.ao_servers                ao  on sc.ao_server                = ao.server
+  inner join public.servers                   se  on ao.server                   = se.pkey
+  inner join public.operating_system_versions osv on se.operating_system_version = osv.pkey;
+
+revoke all    on management."remove-ssl-certificate" from aoadmin;
+grant  select on management."remove-ssl-certificate" to   aoadmin;

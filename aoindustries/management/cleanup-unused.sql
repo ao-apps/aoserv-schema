ff16969983dc493c57d389293ef8e73b9a369b73
Dan Armstrong <dan@aoindustries.com>
2018-11-12 07:24:55 +0000

    Also consider cleaning MySQL and PostgreSQL instances that have either no users or no databases.

--- aoindustries/management/cleanup-unused.sql
+++ aoindustries/management/cleanup-unused.sql
@@ -11,12 +11,12 @@ as
 -- TODO: mysql_server_users (without any mysql_db_users)
 -- TODO: mysql_users (orphaned)
 -- mysql_servers
-          (select 'mysql_servers', count(*) as unused from management."remove-mysql-server" where num_databases=0 and num_users=0)
+          (select 'mysql_servers', count(*) as unused from management."remove-mysql-server" where num_databases=0 or num_users=0)
 -- TODO: postgres_databases (without any postgres_users in same business?/package?)
 -- TODO: postgres_server_users (without any postgres_databases in same business?/package?)
 -- TODO: postgres_users (orphaned)
 -- postgres_servers
-union all (select 'postgres_servers', count(*) as unused from management."remove-postgresql-server" where num_databases=0 and num_users=0)
+union all (select 'postgres_servers', count(*) as unused from management."remove-postgresql-server" where num_databases=0 or num_users=0)
 -- TODO: sendmail_servers without any sendmail_binds
 -- TODO: ssl_certificates (once have auto-cleanup within aoserv-daemon)
 -- TODO: firewalld_zones

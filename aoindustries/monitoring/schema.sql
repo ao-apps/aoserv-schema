5bb577679324b80c287f1f4b9b5e26c761816969
Dan Armstrong <dan@aoindustries.com>
2018-11-18 07:42:19 +0000

    added schema monitoring

---
+++ aoindustries/monitoring/schema.sql
@@ -0,0 +1,9 @@
+-- Depends on:
+-- public
+
+create schema if not exists monitoring;
+
+comment on schema monitoring is 'The monitoring schema contains monitoring configuration';
+
+grant all   on schema monitoring to aoadmin;
+grant usage on schema monitoring to aoserv_app;

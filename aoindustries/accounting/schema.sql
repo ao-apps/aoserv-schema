5ef087f52b2df6bf591b7ca1c1584ecc2b16913d
Dan Armstrong <dan@aoindustries.com>
2018-11-18 06:42:27 +0000

    added schema accounting

--- aoindustries/accounting/schema.sql
+++ aoindustries/accounting/schema.sql
@@ -1,1 +1,6 @@
+-- Depends on:
+-- public
+
 create schema if not exists accounting;
+
+comment on schema accounting is 'The accounting schema is used for AO Industries, Inc. internal accounting processes';

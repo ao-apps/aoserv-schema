3fa1ae3036070b1d983e7159181eba720c1a819e
Dan Armstrong <dan@aoindustries.com>
2018-11-18 07:30:33 +0000

    Added schema permissions.

--- aoindustries/accounting/schema.sql
+++ aoindustries/accounting/schema.sql
@@ -4,3 +4,6 @@
 create schema if not exists accounting;
 
 comment on schema accounting is 'The accounting schema is used for AO Industries, Inc. internal accounting processes';
+	
+grant all   on schema accounting to aoadmin;
+grant usage on schema accounting to aoserv_app;

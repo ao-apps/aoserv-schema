407806a339a248fc92b272f9cf3e6b4a5ce35c8b
Dan Armstrong <dan@aoindustries.com>
2018-11-18 10:45:59 +0000

    added schema management

--- aoindustries/management/schema.sql
+++ aoindustries/management/schema.sql
@@ -1,1 +1,8 @@
+-- Depends on:
+-- public
+
 create schema if not exists management;
+
+comment on schema management is 'The management schema is used for AO professional management service';
+
+grant all on schema management to aoadmin;

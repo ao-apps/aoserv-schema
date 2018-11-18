64b468ccb930af6816bf1c7769edf1d6de4d86c7
Dan Armstrong <dan@aoindustries.com>
2018-11-18 07:35:28 +0000

    added schema schema

---
+++ aoindustries/schema/schema.sql
@@ -0,0 +1,9 @@
+-- Depends on:
+-- None
+
+create schema if not exists "schema";
+
+comment on schema "schema" is 'The "schema" schema contains the definitions of the AOServ data structions over time';
+	
+grant all   on schema "schema" to aoadmin;
+grant usage on schema "schema" to aoserv_app;

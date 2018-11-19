94ed680b8d38ef4185c7a888207363a7396426f6
Dan Armstrong <dan@aoindustries.com>
2018-11-19 05:52:40 +0000

    Unique constraint for very rudimentary duplication detection.

--- aoindustries/schema/Table-index.sql
+++ aoindustries/schema/Table-index.sql
@@ -1,9 +1,6 @@
 create index "Table_schema_idx" on "schema"."Table" (
   "schema"
 );
-create index "Table_sinceVersion_idx" on "schema"."Table" (
-  "sinceVersion"
-);
 create index "Table_lastVersion_idx" on "schema"."Table" (
   "lastVersion"
 );

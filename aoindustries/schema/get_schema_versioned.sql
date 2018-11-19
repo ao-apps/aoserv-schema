26914bbbb5875e150eca7f08c437be68d273d3ff
Dan Armstrong <dan@aoindustries.com>
2018-11-19 05:07:18 +0000

    Function used to lookup schemas by version ranges.

---
+++ aoindustries/schema/get_schema_versioned.sql
@@ -0,0 +1,34 @@
+create or replace function "schema".get_schema_versioned (
+  "name" text,
+  "sinceVersion" text,
+  "lastVersion" text
+)
+returns integer
+as '
+  select
+    s.id
+  from
+               "schema"."Schema"             s
+    inner join "schema"."AOServProtocol" since on s."sinceVersion" = since.version
+    left  join "schema"."AOServProtocol"  last on s."lastVersion"  =  last.version
+  where
+    s."name" = $1
+    and (select created from "schema"."AOServProtocol" where version= $2 ) >= since.created
+    and (
+      case
+        when $3 is null then (select created from "schema"."AOServProtocol" order by created desc limit 1)
+        else (select created from "schema"."AOServProtocol" where version= $3 )
+      end
+      <=
+      case
+        when last.version is null then (select created from "schema"."AOServProtocol" order by created desc limit 1)
+        else last.created
+      end
+    )
+  ;
+'
+language 'sql';
+
+-- Examples
+-- select "schema".get_schema_versioned('public', '1.0a100', null);
+-- Expect: 0

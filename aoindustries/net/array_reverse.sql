3efbad80cbd6b8055fa973d8efc0f8af4d428edc
Dan Armstrong <dan@aoindustries.com>
2018-11-21 06:18:06 +0000

    There are no more shared functions.  Removed unused and improved reverse_hostname greatly.

---
+++ aoindustries/net/array_reverse.sql
@@ -0,0 +1,8 @@
+-- See https://wiki.postgresql.org/wiki/Array_reverse
+CREATE OR REPLACE FUNCTION net.array_reverse(anyarray) RETURNS anyarray AS $$
+SELECT ARRAY(
+    SELECT $1[i]
+    FROM generate_subscripts($1,1) AS s(i)
+    ORDER BY i DESC
+);
+$$ LANGUAGE 'sql' STRICT IMMUTABLE;
---
+++ aoindustries/net/reverse_hostname.sql
@@ -0,0 +1,19 @@
+/**********************************************************************
+ *
+ * Takes a host/domain name and switches the order by labels split by
+ * periods (.)
+ *
+ * $1  the host/domain name
+ *
+ * returns a text with the name parts reversed
+ *
+ *********************************************************************/
+create or replace function net.reverse_hostname (text)
+  returns text
+  as '
+    select array_to_string(net.array_reverse(string_to_array($1, ''.'')), ''.'');
+  '
+  language 'sql'
+  strict
+  immutable
+;

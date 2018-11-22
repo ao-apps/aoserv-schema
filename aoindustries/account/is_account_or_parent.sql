242127eb59e85946bc9e4e423316c0d2897f911d
Dan Armstrong <dan@aoindustries.com>
2018-11-22 03:45:03 +0000

    Renamed is_business_or_parent to is_account_or_parent.

--- aoindustries/account/is_account_or_parent.sql
+++ aoindustries/account/is_account_or_parent.sql
@@ -1,13 +1,13 @@
 /**********************************************************************
  *
- * Checks if a <code>business</code> is the same as, or the parent of
- * another <code>business</code>.
+ * Checks if a <code>account.Account</code> is the same as, or the parent of
+ * another <code>account.Account</code>.
  *
- * $1  the accounting code of the business
- * $2  the accounting code of the business or parent
+ * $1  the accounting code of the account.Account
+ * $2  the accounting code of the account.Account or parent
  *
  *********************************************************************/
-create or replace function account.is_business_or_parent (text,text)
+create or replace function account.is_account_or_parent (text,text)
   returns boolean
   as '
     select
@@ -18,7 +18,7 @@ create or replace function account.is_business_or_parent (text,text)
         then
           false
         else
-          account.is_business_or_parent( $1 , (select parent from account."Account" where accounting = $2 ))
+          account.is_account_or_parent( $1 , (select parent from account."Account" where accounting = $2 ))
         end
       )
     ;

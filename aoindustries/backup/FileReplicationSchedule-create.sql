0137bbd2d7bcca215e9e70c9c6d930a28071be76
Dan Armstrong <dan@aoindustries.com>
2018-11-22 05:09:46 +0000

    Renamed failover_file_schedule to backup.FileReplicationSchedule.

---
+++ aoindustries/backup/FileReplicationSchedule-create.sql
@@ -0,0 +1,25 @@
+create sequence         backup."FileReplicationSchedule_pkey_seq" cycle;
+grant all            on backup."FileReplicationSchedule_pkey_seq" to aoadmin;
+grant select, update on backup."FileReplicationSchedule_pkey_seq" to aoserv_app;
+
+create table backup."FileReplicationSchedule" (
+  pkey integer
+    default nextval('backup."FileReplicationSchedule_pkey_seq"')
+    primary key,
+  replication integer
+    not null,
+  "hour" smallint
+    not null
+    constraint hour_chk
+      check ("hour" >= 0 and "hour" <= 23),
+  "minute" smallint
+    not null
+    constraint minute_chk
+      check ("minute" >= 0 and "minute" <= 59),
+  enabled boolean
+    not null
+    default true,
+  unique(replication, "hour", "minute")
+);
+grant all                    on backup."FileReplicationSchedule" to aoadmin;
+grant select, insert, delete on backup."FileReplicationSchedule" to aoserv_app;
--- aoindustries/backup/FileReplicationSchedule-reference.sql
+++ aoindustries/backup/FileReplicationSchedule-reference.sql
@@ -1,4 +1,4 @@
-alter table failover_file_schedule
+alter table backup."FileReplicationSchedule"
   add constraint replication_fkey
   foreign key (replication)
   references backup."FileReplication" (pkey)

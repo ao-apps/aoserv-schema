e243bbec7660f1a568aa0cd0b0c7d6560f1ad1f1
Dan Armstrong <dan@aoindustries.com>
2018-11-23 02:20:53 +0000

    TODO: Fix this risky default of all servers.

--- aoindustries/server/master_servers-create.sql
+++ aoindustries/server/master_servers-create.sql
@@ -2,6 +2,13 @@ create sequence         master.master_servers_pkey_seq cycle;
 grant all            on master.master_servers_pkey_seq to aoadmin;
 grant select, update on master.master_servers_pkey_seq to aoserv_app;
 
+/*
+TODO: When a master.MasterUser has no servers listed, it is considered a
+superuser with access to all servers and all data.  While this is clever, it
+is clever in the way a SQL DELETE will remove all data when you forget the
+WHERE clause.  This is too risky.  Find a better way, roles or another flag
+in master.MasterUser.
+ */
 create table master_servers (
   pkey integer
     default nextval('master.master_servers_pkey_seq')

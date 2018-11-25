ba0390d361059219a0a5b0a8e0dda29b543f2718
Dan Armstrong <dan@aoindustries.com>
2018-11-25 00:32:09 +0000

    Now validating and returning message or null instead of throwing exceptions.

---
+++ aoindustries/net/reputation/validateReputationIdentifier-function.sql
@@ -0,0 +1,35 @@
+/*
+Checks a reputation identifier.
+
+Returns NULL when valid.
+Returns a description when invalid.
+
+ipset has maximum name length of 31, and we prefix four characters, leaving 27 characters max.
+  Prefixes:
+    IpReputationSet:
+      character 0: 'R': Reputation
+      character 1: 'G': Good,      'B': Bad
+      character 2: 'U': Uncertain, 'D': Definite, 'N': Network
+      character 3: '_': Separator, 'N': New list (for loading while old copy active)
+    IpSet:
+      character 0: 'I': IP Set
+      character 1: 'H': Hosts, 'N': Networks, 'U': Union (rules should normally use the union)
+      character 2: '_': Reserved
+      character 3: '_': Separator, 'N': New list (for loading while old copy active)
+ */
+CREATE OR REPLACE FUNCTION "net/reputation"."validateReputationIdentifier"(identifier text)
+RETURNS text AS $$
+DECLARE
+  _validate_ipset_result text := (SELECT net."validateIpSetIdentifier"(identifier));
+BEGIN
+  IF NOT LENGTH(identifier) BETWEEN 1 AND 31 THEN
+    RETURN 'Reputation identifiers must have between 1 and 27 characters: ''' || identifier || '''';
+  END IF;
+  IF _validate_ipset_result IS NOT NULL THEN
+    RETURN _validate_ipset_result;
+  END IF;
+  -- All is OK
+  RETURN null;
+END;
+$$ LANGUAGE plpgsql
+IMMUTABLE;
---
+++ aoindustries/net/reputation/validateReputationLimits-function.sql
@@ -0,0 +1,143 @@
+/*
+Checks reputation limits.
+
+Returns NULL when valid.
+Returns a description when invalid.
+
+Based on reputation, traffic is divided into six classes:
+  gm: manual good
+  bm: manual bad
+  bd: definite bad
+  bu: uncertain bad
+  gu: uncertain good
+  gd: definite good
+  gn: network good
+  uu: unknown
+
+For each class, we configure:
+  per-ip max syn/sec
+  max syn/sec
+  per-ip max packets/sec
+  max packets/sec
+
+Any limit of 0 means to just drop outright
+
+syn put before packets/sec so that synflood doesn't affect packets/second
+for non-syn (established connections) within the same class.
+
+Per-IP limits are added first, so that a massive attack from one IP source will
+not block all members of that class.
+ */
+CREATE OR REPLACE FUNCTION "net/reputation"."validateReputationLimits" (
+  "class"             text,
+  syn_per_ip_burst    smallint,
+  syn_per_ip_rate     smallint,
+  syn_per_ip_unit     text,
+  syn_per_ip_size     smallint,
+  syn_burst           smallint,
+  syn_rate            smallint,
+  syn_unit            text,
+  packet_per_ip_burst integer,
+  packet_per_ip_rate  integer,
+  packet_per_ip_unit  text,
+  packet_per_ip_size  integer,
+  packet_burst        integer,
+  packet_rate         integer,
+  packet_unit         text
+)
+RETURNS text AS $$
+BEGIN
+  -- Check bounds for each parameter
+  -- syn_per_ip
+  -- TODO: Make an enum both in PostgreSQL and Java
+  IF syn_per_ip_unit NOT IN ('second', 'minute', 'hour', 'day') THEN
+    RETURN "class" || ' time unit invalid for syn/IP, must be one of ''second'', ''minute'', ''hour'', or ''day'': ' || syn_per_ip_unit;
+  END IF;
+  IF NOT syn_per_ip_burst BETWEEN 0 AND 10000 THEN
+    RETURN "class" || ' syn/' || syn_per_ip_unit || '/IP burst outside of range, must be between 0 and 10000: ' || syn_per_ip_burst;
+  END IF;
+  IF NOT syn_per_ip_rate BETWEEN 0 AND 10000 THEN
+    RETURN "class" || ' syn/' || syn_per_ip_unit || '/IP outside of range, must be between 0 and 10000: ' || syn_per_ip_rate;
+  END IF;
+  IF NOT (syn_per_ip_size=0 OR syn_per_ip_size BETWEEN 10 AND 10000) THEN
+    RETURN "class" || ' syn/' || syn_per_ip_unit || '/IP size outside of range, must be 0 or between 10 and 10000: ' || syn_per_ip_size;
+  END IF;
+  -- syn
+  -- TODO: Make an enum both in PostgreSQL and Java
+  IF syn_unit NOT IN ('second', 'minute', 'hour', 'day') THEN
+    RETURN "class" || ' time unit invalid for syn, must be one of ''second'', ''minute'', ''hour'', or ''day'': ' || syn_unit;
+  END IF;
+  IF NOT syn_burst BETWEEN 0 AND 10000 THEN
+    RETURN "class" || ' syn/' || syn_unit || ' burst outside of range, must be between 0 and 10000: ' || syn_burst;
+  END IF;
+  IF NOT syn_rate BETWEEN 0 AND 10000 THEN
+    RETURN "class" || ' syn/' || syn_unit || ' outside of range, must be between 0 and 10000: ' || syn_rate;
+  END IF;
+  -- packet_per_ip
+  IF packet_per_ip_unit NOT IN ('second', 'minute', 'hour', 'day') THEN
+    RETURN "class" || ' time unit invalid for packet/IP, must be one of ''second'', ''minute'', ''hour'', or ''day'': ' || packet_per_ip_unit;
+  END IF;
+  IF NOT packet_per_ip_burst BETWEEN 0 AND 100000 THEN
+    RETURN "class" || ' packet/' || packet_per_ip_unit || '/IP burst outside of range, must be between 0 and 100000: ' || packet_per_ip_burst;
+  END IF;
+  IF NOT packet_per_ip_rate BETWEEN 0 AND 100000 THEN
+    RETURN "class" || ' packet/' || packet_per_ip_unit || '/IP outside of range, must be between 0 and 100000: ' || packet_per_ip_rate;
+  END IF;
+  IF NOT (packet_per_ip_size=0 OR packet_per_ip_size BETWEEN 10 AND 10000) THEN
+    RETURN "class" || ' packet/' || packet_per_ip_unit || '/IP size outside of range, must be 0 or between 10 and 10000: ' || packet_per_ip_size;
+  END IF;
+  -- packet
+  -- TODO: Make an enum both in PostgreSQL and Java
+  IF packet_unit NOT IN ('second', 'minute', 'hour', 'day') THEN
+    RETURN "class" || ' time unit invalid for packet, must be one of ''second'', ''minute'', ''hour'', or ''day'': ' || packet_unit;
+  END IF;
+  IF NOT packet_burst BETWEEN 0 AND 100000 THEN
+    RETURN "class" || ' packet/' || packet_unit || ' burst outside of range, must be between 0 and 100000: ' || packet_burst;
+  END IF;
+  IF NOT packet_rate BETWEEN 0 AND 100000 THEN
+    RETURN "class" || ' packet/' || packet_unit || ' outside of range, must be between 0 and 10000: ' || packet_rate;
+  END IF;
+
+  -- The special case of zero must match for all limits within a class
+  IF NOT (
+    (
+          syn_per_ip_burst     = 0
+      AND syn_per_ip_rate      = 0
+      AND syn_per_ip_size      = 0
+      AND syn_burst            = 0
+      AND syn_rate             = 0
+      AND packet_per_ip_burst  = 0
+      AND packet_per_ip_rate   = 0
+      AND packet_per_ip_size   = 0
+      AND packet_burst         = 0
+      AND packet_rate          = 0
+    ) OR (
+          syn_per_ip_burst    != 0
+      AND syn_per_ip_rate     != 0
+      AND syn_per_ip_size     != 0
+      AND syn_burst           != 0
+      AND syn_rate            != 0
+      AND packet_per_ip_burst != 0
+      AND packet_per_ip_rate  != 0
+      AND packet_per_ip_size  != 0
+      AND packet_burst        != 0
+      AND packet_rate         != 0
+    )
+  ) THEN
+    RETURN "class" || ' burst and rate must be all zero or all non-zero';
+  END IF;
+
+  -- per-IP must be <= max (do same for both burst and rate)
+  -- To implement would require unit conversions for comparison:
+  -- IF syn_per_ip_rate > syn_rate THEN
+  --   RETURN "class" || ' syn/second/IP may not be greater than syn/second: ' || sps_per_ip || ' > ' || sps;
+  -- END IF;
+  -- IF pps_per_ip > pps THEN
+  --   RETURN "class" || ' packets/second/IP may not be greater than packets/second: ' || pps_per_ip || ' > ' || pps;
+  -- END IF;
+
+  -- All is OK
+  RETURN null;
+END;
+$$ LANGUAGE plpgsql
+IMMUTABLE;

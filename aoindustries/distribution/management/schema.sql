8565b0a779c579dad00a8f09f8cc1a5ea5dc46ac
Dan Armstrong <dan@aoindustries.com>
2018-11-26 21:58:01 +0000

    Updated schema permissions for moved tables.

--- aoindustries/distribution/management/schema.sql
+++ aoindustries/distribution/management/schema.sql
@@ -2,4 +2,5 @@ create schema if not exists "distribution.management";
 
 comment on schema "distribution.management" is 'AO professional management service';
 
-grant all on schema "distribution.management" to aoadmin;
+grant all   on schema "distribution.management" to aoadmin;
+grant usage on schema "distribution.management" to aoserv_app;
--- aoindustries/management/schema.sql
+++ aoindustries/management/schema.sql
@@ -3,4 +3,3 @@ create schema if not exists management;
 comment on schema management is 'AO professional management service';
 
 grant all   on schema management to aoadmin;
-grant usage on schema management to aoserv_app;

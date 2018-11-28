03f4df94978eca246b7ea68b1eed8b4b87fa7d80
Dan Armstrong <dan@aoindustries.com>
2018-11-28 15:19:08 +0000

    Already done.

--- aoindustries/email/AttachmentBlock-create.sql
+++ aoindustries/email/AttachmentBlock-create.sql
@@ -2,7 +2,6 @@ create sequence         email."AttachmentBlock_id_seq" cycle;
 grant all            on email."AttachmentBlock_id_seq" to aoadmin;
 grant select, update on email."AttachmentBlock_id_seq" to aoserv_app;
 
--- TODO: Rename to singular
 create table email."AttachmentBlock" (
   id integer
     default nextval('email."AttachmentBlock_id_seq"')

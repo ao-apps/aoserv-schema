c78c33ceefe88c761dca4a99d613208254fb69bb
Dan Armstrong <dan@aoindustries.com>
2018-11-29 01:35:54 +0000

    Granted all usage on public schema.

--- aoindustries/public/schema.sql
+++ aoindustries/public/schema.sql
@@ -1,11 +1,14 @@
--- TODO: Drop schema once conversion done and unused
---       And leave drop here, since it will be our standard to not have the default public schema.
-
 -- Note: Drop from "postgres" superuser once to get ownership of the public schema
 create schema if not exists public;
 
--- Note: This has to be set by the "postgres" superuser
 comment on schema public is 'The default schema used for things not otherwise classified into a specific schema';
 
 grant  all    on schema public to aoadmin;
 grant  usage  on schema public to aoserv_app;
+grant  usage  on schema public to accounting;
+grant  usage  on schema public to billing;
+grant  usage  on schema public to distribution;
+grant  usage  on schema public to infrastructure;
+grant  usage  on schema public to management;
+grant  usage  on schema public to monitoring;
+grant  usage  on schema public to reseller;

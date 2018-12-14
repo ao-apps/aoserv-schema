e8b909ac40f1c9849ecc4ed3b3611efbb65bd49d
Dan Armstrong <dan@aoindustries.com>
2018-12-14 10:27:05 +0000

    Completed conversion.

--- aoindustries/web/RewriteRule-create.sql
+++ aoindustries/web/RewriteRule-create.sql
@@ -21,27 +21,3 @@ create table web."RewriteRule" (
 );
 grant all            on web."RewriteRule" to aoadmin;
 grant select, delete on web."RewriteRule" to aoserv_app;
-
--- Conversion
-insert into web."RewriteRule"
-select
-  r.id,
-  r.httpd_site_bind,
-  r.sort_order,
-  r.pattern,
-  r.substitution,
-  case
-    when r.substitution='-' then 'L'
-    when r.no_escape and nh.operating_system_version = 70 then 'END,NE,R=permanent'
-    when r.no_escape and nh.operating_system_version = 67 then 'L,NE,R=permanent'
-    when !r.no_escape and nh.operating_system_version = 70 then 'END,R=permanent'
-    when !r.no_escape and nh.operating_system_version = 67 then 'L,R=permanent'
-    else '' -- Intentionally invalid to catch error
-  end,
-  r.comment
-  -- nh.operating_system_version
-from
-  web."Redirect_20181214" r
-  inner join web."VirtualHost" vh on r.httpd_site_bind = vh.id
-  inner join web."Site" ws on vh.httpd_site = ws.id
-  inner join net."Host" nh on ws.ao_server = nh.id;

bdaa6777178dbeed31a1e8368ecf57a963e58f2d
Dan Armstrong <dan@aoindustries.com>
2018-12-14 12:23:18 +0000

    Protocols are now upper-case and enum.

--- aoindustries/net/AppProtocol-data1.sql
+++ aoindustries/net/AppProtocol-data1.sql
@@ -1,60 +1,60 @@
 COPY net."AppProtocol" FROM stdin;
-submission	587	Mail Message Submission	t	tcp
-NTP	123	Network Time Protocol	f	udp
-SMTP	25	Simple Mail Transport Protocol	t	tcp
-ldap	389	LDAP	f	tcp
-HTTP	80	Hypertext Transfer Protocol	f	tcp
-auth	113	Identity service	f	tcp
-HTTPS	443	Secure Hypertext Transfer Protocol	f	tcp
-shell	514	Remote Shell	f	tcp
-MySQL	3306	MySQL	f	tcp
-PostgreSQL	5432	PostgreSQL	f	tcp
-Xvfb	6000	X Virtual Frame Buffer	f	tcp
-ajp12	8007	Apache JServ Protocol Version 1.2	f	tcp
-ajp13	8009	Apache JServ Protocol Version 1.3	f	tcp
-RMI	4444	Java RMI	f	tcp
-JMX	8082	JBoss JMX	f	tcp
-hypersonic	1476	HypersonicSQL	f	tcp
-webserver	8083	JBoss Web Server	f	tcp
-JNP	1099	Java JNP	f	tcp
-tomcat4-shutdown	8005	Jakarta Tomcat 4 Shutdown Port	f	tcp
-user	65535	User port	f	tcp
-ozone-admin	3000	Ozone Administration	f	tcp
-ozone	3333	Ozone	f	tcp
-bootpc	68	BOOTP Client	f	udp
-bootps	67	BOOTP Server	f	udp
-irc	6565	IRC	f	tcp
-aoserv-daemon-ssl	984	AO Serv Daemon (SSL)	f	tcp
-rquotad	805	rpc.rquotad	f	tcp
-unknown	1024	Unknown	f	tcp
-aoserv-master-ssl	4583	AO Serv Master (SSL)	f	tcp
-aoserv-daemon	985	AO Serv Daemon	f	tcp
-aoserv-master	4582	AO Serv Master	f	tcp
-FTP	21	File Transfer Protocol	t	tcp
-SSH	22	Secure Shell	t	tcp
-Telnet	23	Telnet	t	tcp
-POP3	110	Post Office Protocol Version 3	t	tcp
-IMAP2	143	IMAP Version 2	t	tcp
-SIMAP	993	Secure IMAP	t	tcp
-SPOP3	995	Secure Post Office Protocol Version 3	t	tcp
-cvspserver	2401	CVS pserver	t	tcp
-FTP-DATA	20	File Transfer Protocol Data	t	tcp
-DNS	53	Domain Name Service	f	udp
-sunrpc	111	Sun Remote Procedure Call	f	udp
-nfs	2049	NFS	f	udp
-netbios-dgm	138	NetBIOS DGM	f	udp
-netbios-ns	137	NetBIOS NS	f	udp
-netbios-ssn	139	NetBIOS SSN	f	udp
-rndc	953	rndc control sockets (BIND 9)	f	tcp
-talk	517	talk	f	udp
-ntalk	518	ntalk	f	udp
-milter	12000	Sendmail Milter	f	tcp
-sieve	4190	timsieved	f	tcp
-RFB	5900	RFB used by VNC	f	tcp
+submission	587	Mail Message Submission	t	TCP
+NTP	123	Network Time Protocol	f	UDP
+SMTP	25	Simple Mail Transport Protocol	t	TCP
+ldap	389	LDAP	f	TCP
+HTTP	80	Hypertext Transfer Protocol	f	TCP
+auth	113	Identity service	f	TCP
+HTTPS	443	Secure Hypertext Transfer Protocol	f	TCP
+shell	514	Remote Shell	f	TCP
+MySQL	3306	MySQL	f	TCP
+PostgreSQL	5432	PostgreSQL	f	TCP
+Xvfb	6000	X Virtual Frame Buffer	f	TCP
+ajp12	8007	Apache JServ Protocol Version 1.2	f	TCP
+ajp13	8009	Apache JServ Protocol Version 1.3	f	TCP
+RMI	4444	Java RMI	f	TCP
+JMX	8082	JBoss JMX	f	TCP
+hypersonic	1476	HypersonicSQL	f	TCP
+webserver	8083	JBoss Web Server	f	TCP
+JNP	1099	Java JNP	f	TCP
+tomcat4-shutdown	8005	Jakarta Tomcat 4 Shutdown Port	f	TCP
+user	65535	User port	f	TCP
+ozone-admin	3000	Ozone Administration	f	TCP
+ozone	3333	Ozone	f	TCP
+bootpc	68	BOOTP Client	f	UDP
+bootps	67	BOOTP Server	f	UDP
+irc	6565	IRC	f	TCP
+aoserv-daemon-ssl	984	AO Serv Daemon (SSL)	f	TCP
+rquotad	805	rpc.rquotad	f	TCP
+unknown	1024	Unknown	f	TCP
+aoserv-master-ssl	4583	AO Serv Master (SSL)	f	TCP
+aoserv-daemon	985	AO Serv Daemon	f	TCP
+aoserv-master	4582	AO Serv Master	f	TCP
+FTP	21	File Transfer Protocol	t	TCP
+SSH	22	Secure Shell	t	TCP
+Telnet	23	Telnet	t	TCP
+POP3	110	Post Office Protocol Version 3	t	TCP
+IMAP2	143	IMAP Version 2	t	TCP
+SIMAP	993	Secure IMAP	t	TCP
+SPOP3	995	Secure Post Office Protocol Version 3	t	TCP
+cvspserver	2401	CVS pserver	t	TCP
+FTP-DATA	20	File Transfer Protocol Data	t	TCP
+DNS	53	Domain Name Service	f	UDP
+sunrpc	111	Sun Remote Procedure Call	f	UDP
+nfs	2049	NFS	f	UDP
+netbios-dgm	138	NetBIOS DGM	f	UDP
+netbios-ns	137	NetBIOS NS	f	UDP
+netbios-ssn	139	NetBIOS SSN	f	UDP
+rndc	953	rndc control sockets (BIND 9)	f	TCP
+talk	517	talk	f	UDP
+ntalk	518	ntalk	f	UDP
+milter	12000	Sendmail Milter	f	TCP
+sieve	4190	timsieved	f	TCP
+RFB	5900	RFB used by VNC	f	TCP
 \.
-insert into net."AppProtocol" values ('SMTPS', 465, 'SMTP SSL', false, 'tcp');
-insert into net."AppProtocol" values ('spamd', 783, 'SpamAssassin Daemon', false, 'tcp');
+insert into net."AppProtocol" values ('SMTPS', 465, 'SMTP SSL', false, 'TCP');
+insert into net."AppProtocol" values ('spamd', 783, 'SpamAssassin Daemon', false, 'TCP');
 -- TODO: memcached also listens on UDP, how to represent in net.AppProtocol table?
-insert into net."AppProtocol" values ('memcached', 11211, 'Memcached', false, 'tcp');
-insert into net."AppProtocol" values ('csync2', 30865, 'Csync2 Daemon', false, 'tcp');
-insert into net."AppProtocol" values ('elasticsearch', 9200, 'Elasticsearch', false, 'tcp');
+insert into net."AppProtocol" values ('memcached', 11211, 'Memcached', false, 'TCP');
+insert into net."AppProtocol" values ('csync2', 30865, 'Csync2 Daemon', false, 'TCP');
+insert into net."AppProtocol" values ('elasticsearch', 9200, 'Elasticsearch', false, 'TCP');

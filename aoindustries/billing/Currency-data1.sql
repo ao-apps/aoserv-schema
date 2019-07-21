906626b5c3cd343c734cc842559f400b83de2cad
Dan Armstrong <dan@aoindustries.com>
2019-07-21 08:50:10 +0000

    Added Canadian pesos.

--- aoindustries/billing/Currency-data1.sql
+++ aoindustries/billing/Currency-data1.sql
@@ -2,4 +2,5 @@ begin;
 insert into billing."Currency" values ('USD', 2, '30.00');
 insert into billing."Currency" values ('EUR', 2, '30.00');
 insert into billing."Currency" values ('JPY', 0, '3000');
+insert into billing."Currency" values ('CAD', 2, '30.00');
 commit;

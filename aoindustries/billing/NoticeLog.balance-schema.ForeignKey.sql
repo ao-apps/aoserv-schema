3a305c1edebedb21cd6860a6b8054ce66433908e
Dan Armstrong <dan@aoindustries.com>
2019-07-21 06:44:40 +0000

    Fixed foreign key table name.

--- aoindustries/billing/NoticeLog.balance-schema.ForeignKey.sql
+++ aoindustries/billing/NoticeLog.balance-schema.ForeignKey.sql
@@ -1,1 +1,1 @@
-select "schema".add_foreign_key('billing', 'NoticeLog.balance', 'noticeLog', 'billing', 'notice_types', 'pkey', '1.83.0', null);
+select "schema".add_foreign_key('billing', 'NoticeLog.balance', 'noticeLog', 'billing', 'notice_log', 'pkey', '1.83.0', null);

4d1ea60680e16c5e04dbd1dcf935190982feacb0
Dan Armstrong <dan@aoindustries.com>
2019-07-25 07:18:44 +0000

    Improved wording.

--- aoindustries/email/BlackholeAddress-aosh.Command.sql
+++ aoindustries/email/BlackholeAddress-aosh.Command.sql
@@ -2,7 +2,7 @@ select aosh.add_command(
   'remove_blackhole_email_address',
   'email',
   'blackhole_email_addresses',
-  'stops emails from being discarded sent to /dev/null',
+  'stops emails from being discarded to /dev/null',
   '<i>address</i> <i>ao_server</i>',
   '1.0a100',
   null

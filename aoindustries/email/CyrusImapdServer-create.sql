643763a8e62ec38a0433ac1ba86dacdb662e3f6d
Dan Armstrong <dan@aoindustries.com>
2019-08-15 01:14:13 +0000

    Delated delete seems unreliable so far It is difficult to delete folders and millions of lock files accumulating under /var/lib/imap/lock/u/DELETED/user/*/**/*.lock Turned-off on all servers and removed defaults

--- aoindustries/email/CyrusImapdServer-create.sql
+++ aoindustries/email/CyrusImapdServer-create.sql
@@ -13,16 +13,22 @@ create table email."CyrusImapdServer" (
   allow_plaintext_auth boolean not null
     -- See also CyrusImapdServer.DEFAULT_ALLOW_PLAINTEXT_AUTH
     default false,
+  -- Delated delete seems unreliable so far
+  --
+  -- It is difficult to delete folders and millions of lock files accumulating
+  -- under /var/lib/imap/lock/u/DELETED/user/*/**/*.lock
+  --
+  -- Turned-off on all servers and removed defaults
   delete_duration real
     -- See also CyrusImapdServer.DEFAULT_DELETE_DURATION
-    default 3
+    --default 3
     check (
       delete_duration is null
       or delete_duration > 0
     ),
   delete_duration_unit email."CyrusImapdServer.TimeUnit"
     -- See also CyrusImapdServer.DEFAULT_DELETE_DURATION_UNIT
-    default 'd'
+    --default 'd'
     check (
       delete_duration_unit is null
       or delete_duration is not null

f5f3ea954ce66701fc38326bc86863e00389a98e
Dan Armstrong <dan@aoindustries.com>
2019-09-15 00:56:43 +0000

    New currency "MAD" for Morocco.

--- aoindustries/billing/Currency-data1.sql
+++ aoindustries/billing/Currency-data1.sql
@@ -3,5 +3,5 @@ insert into billing."Currency" values ('USD', 2,  30.00);
 insert into billing."Currency" values ('EUR', 2,  30.00);
 insert into billing."Currency" values ('JPY', 0,   3000);
 insert into billing."Currency" values ('CAD', 2,  30.00);
-insert into billing."Currency" values ('MAD', 2, 300.00)
+insert into billing."Currency" values ('MAD', 2, 300.00);
 commit;

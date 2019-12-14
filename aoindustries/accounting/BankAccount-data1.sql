7546c7ee31f022b1d20cd74f4b84c80b45d559f0
Dan Armstrong <dan@aoindustries.com>
2019-12-14 06:26:07 +0000

    New account: Wells Fargo in Payson, Arizona

--- aoindustries/accounting/BankAccount-data1.sql
+++ aoindustries/accounting/BankAccount-data1.sql
@@ -1,4 +1,5 @@
 begin;
 insert into accounting."BankAccount" values ('compass_account', 'Compass Bank Account', 'compass', 3, 0);
-insert into accounting."BankAccount" values ('southtrust', 'SouthTrust', 'southtrust', 3, 1);
+insert into accounting."BankAccount" values ('southtrust', 'Wells Fargo in Mobile, Alabama', 'southtrust', 3, 1);
+insert into accounting."BankAccount" values ('wells_fargo_az', 'Wells Fargo in Payson, Arizona', 'southtrust', 3, 1);
 commit;
